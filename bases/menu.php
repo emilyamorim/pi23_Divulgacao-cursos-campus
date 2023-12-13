015e11ffbc8cd7eefe0cfc4b85a32db02c8dd908
j0a0vict0r <victor.lima1@escolar.ifrn.edu.br>
2023-12-13 15:45:21 -0300

    atualizado

--- bases/menu.php
+++ bases/menu.php
@@ -3,6 +3,9 @@
         <a href=""><img src="src/imagens/iconecang.png" width="200"></a>
     </div>
     <ul class="menu">
+        <li id="login"><a href="cadastro.php">Cadastro</a></li>
         <li id="login"><a href="login.php">Login</a></li>
+        <li id="login"><a href="perfil.php">Perfil</a></li>
+        <li id="login"><a href="login.php">Sair</a></li>
     </ul>
 </nav>
